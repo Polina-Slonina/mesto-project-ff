c282c9a3586cc382bfcaad76978e5d8c83177429
Polina Slonina <sloninapp@mail.ru>
2024-12-25 23:25:47 +0300

    настройка валидации и подключение сервера

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     filename: 'main.js',
             publicPath: ''
    },
-   devtool: 'source-map',
+  //  devtool: 'source-map',
     mode: 'development', // добавили режим разработчика
   devServer: {
     static: path.resolve(__dirname, './dist'), // путь, куда "смотрит" режим разработчика
